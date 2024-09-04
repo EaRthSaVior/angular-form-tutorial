6770bb1348945dbe6216bba860855126e1517b01
Teoh Soon Ann <101308101+EaRthSaVior@users.noreply.github.com>
2024-09-04 18:20:47 +0800

    submit and reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,24 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  submitted = false;
   defaultSecret = 'pet';
   answer = '';
   genders = ['Male', 'Female', 'Other'];
+  user = { username: '', email: '', secret: '', answer: '', gender: '' };
   suggestUserName() {
     const suggestedName = 'Superuser';
     // this.signupForm.setValue({});//set for whole form
     this.signupForm.form.patchValue({ userData: { username: suggestedName } }); //update only username field inuserData object
   }
   onSubmit() {
+    this.submitted = true;
     console.log(this.signupForm);
+    this.user.username = this.signupForm.form.value.userData.username;
+    this.user.email = this.signupForm.form.value.userData.email;
+    this.user.secret = this.signupForm.form.value.secret;
+    this.user.answer = this.signupForm.form.value.answer;
+    this.user.gender = this.signupForm.form.value.gender;
+    this.signupForm.reset();
   }
 }
