5ec99709d6e024d95503d2c7dc6171a2e7313a1c
Teoh Soon Ann <101308101+EaRthSaVior@users.noreply.github.com>
2024-09-04 16:51:26 +0800

    single and double way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultSecret = 'pet';
+  answer = '';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
